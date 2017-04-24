a93426af05e0cc0757dd96fc7029430602b2d9ee
Your Name <you@example.com>
2017-04-24 14:15:14 +0900

    update

--- customSetting.js
+++ customSetting.js
@@ -2,7 +2,6 @@
 // this name should be sub-directory name under /var/www/html/
 var project_name = 'juniperSrx_webJs';
 
-// this URL represents this host
-var project_URL = 'fwopen.skplanet.com';
+// javascript source 
 
 
